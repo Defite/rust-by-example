854e321fe11c9aa6da09eadb32c691920a5e009a
Defite <me@defite.ru>
2022-11-06 00:42:25 +0300

    feat(readme, example): add  Fahrenheit to Celsius converter

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rust learning examples
+
+All examples are located in `examples` folder. In order to `rust-analyzer` work correctly, `cd` into folder with example and open it in your IDE.
+
+1) Guessing game - try to guess random number
+2) Fahrenheit to Celsius and vice versa converter
+
+## How to run examples
+
+```bash
+# Skip clone repo step
+cd examples/fahrenheit_celsius
+cargo run
+```
---
+++ examples/fahrenheit_celsius/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    println!("Convert Fahrenheit to Celsius and against");
+
+    let read_input = move |msg: &str| {
+        println!("{}", msg);
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        line.trim().to_string()
+    };
+
+    let temperature: f32 = read_input("Enter temperature")
+        .parse()
+        .expect("Failed to enter number");
+
+    let conversion_type: &str = &read_input("Choose unit Fahrenheit (F) or Celcius(C)");
+
+    println!(
+        "Your temperature: {}",
+        format!("{:.1}", conversion_choice(temperature, conversion_type))
+    );
+}
+
+fn fahrenheit_to_celsius(temperature: f32) -> f32 {
+    return (temperature - 32.0) * 5.0 / 9.0;
+}
+
+fn celsius_to_fahrenheit(temperature: f32) -> f32 {
+    return (temperature * 9.0 / 5.0) + 32.0;
+}
+
+fn conversion_choice(temperature: f32, conversion_type: &str) -> f32 {
+    match conversion_type {
+        "F" => fahrenheit_to_celsius(temperature),
+        "C" => celsius_to_fahrenheit(temperature),
+        _ => 0.0,
+    }
+}
